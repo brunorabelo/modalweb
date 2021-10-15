444d3127376e843c4a739bce3aa66b2298e6e388
Bruno Botelho <brunorabelow@gmail.com>
2021-10-15 14:18:07 +0200

    db

---
+++ db/db.php
@@ -0,0 +1,51 @@
+<?php
+
+class Database
+{
+    public static function connect()
+    {
+        $dsn = 'mysql:dbname=td3;host=127.0.0.1';
+        $user = 'root';
+        $password = '';
+        $dbh = null;
+        try {
+            $dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connexion échouée : ' . $e->getMessage();
+            exit(0);
+        }
+        return $dbh;
+    }
+}
+
+
+//$dbh = Database::connect();
+
+
+class Utilisateur
+{
+    public $login;
+    public $mdp;
+    public $nom;
+    public $prenom;
+    public $promotion;
+    public $naissance;
+    public $email;
+    public $feuille;
+
+
+    public static function getUtilisateur($dbh, $login)
+    {
+        $query = "SELECT * FROM `users` WHERE login=?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Utilisateur');
+        $sth->execute(array($login));
+        $user = $sth->fetch();
+        $sth->closeCursor();
+
+        if (!$user)
+            return null;
+        return $user;
+    }
+}
